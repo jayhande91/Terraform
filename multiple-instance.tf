d906c4feca2fae441b4e9d27e7164125b1996d04
jayhande91 <jayhande91@gmail.com>
2021-08-12 18:21:41 +0530

    update Security group

--- multiple-instance.tf
+++ multiple-instance.tf
@@ -3,6 +3,7 @@ resource "aws_instance" "MyFirstInstnace" {
   count         = 3
   ami           = "ami-04db49c0fb2215364"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [ "sg-0900454cf7115f97b", "sg-0afb63f2184ba978a" ]
 
   tags = {
     Name = "demoinstnce-${count.index}"
